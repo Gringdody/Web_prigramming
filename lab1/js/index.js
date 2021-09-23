79f7058c7c5d540f869a9b092b2f2862d05ebdef
Gringdody <46221144+Gringdody@users.noreply.github.com>
2021-09-23 08:43:25 +0300

    Add files via upload

--- lab1/js/index.js
+++ lab1/js/index.js
@@ -29,9 +29,44 @@ $(function() {
         return validate_coordinate('.x_input') & validate_y() & validate_coordinate('.r_input');
     }
 
+    window.onunload = load_table();
+
+    function load_table(){
+        for(let i=0; i<sessionStorage.length; i++) {
+            let key = sessionStorage.key(i);
+            $('#result_table').append(sessionStorage.getItem(key));
+        }
+    }
+
+    function get_random_int(){
+        return Math.floor(Math.random() * 100);
+    }
+
     $('#form').on('submit', function(event) {
+        if(document.getElementById("errors").rows.length != 0){
+            let table = document.getElementById("errors");
+            let rowCount = table.rows.length;
+            while(table.rows.length > 0) {
+                table.deleteRow(0);
+            }
+        }
         event.preventDefault();
+        document.getElementById("errors_div").innerHTML = "";
+
+        if (!validate_coordinate('.x_input')){
+            document.getElementById('errors_div').innerHTML = '<b>X coordinate error</b>';
+            return;
+        }
+        if (!validate_y()){
+            document.getElementById('errors_div').innerHTML = '<b>Y coordinate error</b>';
+            return;
+        }
+        if (!validate_coordinate('.r_input')){
+            document.getElementById('errors_div').innerHTML = '<b>R coordinate error</b>';
+            return;
+        }
         if (!validateForm()) return;
+
         $.ajax({
             url: 'php/index.php',
             method: 'POST',
@@ -47,6 +82,8 @@ $(function() {
                     answer += '<td>' + data.current_time + '</td>';
                     answer += '<td>' + data.time_execute + '</td>';
                     answer += '<td>' + data.hit_result + '</td>';
+                    let str = get_random_int().toString();
+                    sessionStorage.setItem(str, answer);
                     $('#result_table').append(answer);
                 }
             }
